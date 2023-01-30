df3454c72c1264b87dab025e1e5c9730ac149d7d
Basiel Makled <basiel1234@gmail.com>
2023-01-30 14:32:28 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [pokemon, setPokemon] = useState([]);
+  const [view, setView] = useState(false)
+
+  const retrieve = (e) => {
+    const allpokemon =  pokemon.map((pokemonObj,index)=>{
+      return pokemonObj.name
+    })
+    setPokemon(allpokemon)
+    setView(true)
+    console.log(allpokemon)
+  }
+
+  useEffect(()=>{
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=50')
+      .then(response => {
+        return response .json()
+      })
+      .then(response => {
+        setPokemon(response.results)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={(event)=>{retrieve(event)}}>Fetch Pokemon</button>
+          {
+            //if view has been set to true
+            (view)? 
+            //then do all this stuff
+            pokemon.map((pokemonObj,index)=>{ 
+              return <li>{pokemonObj}</li>
+            })
+            :null
+          }
+      </div>
     </div>
   );
 }
